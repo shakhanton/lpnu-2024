ccaa77c85d7156dd6b8070310977f0910e9ca698
Anton Shakh <shakh@quintagroup.org>
2024-04-23 10:56:49 +0300

    test  On branch master  Changes to be committed: 	modified:   cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -69,7 +69,9 @@ module "cdn" {
   #     acm_certificate_arn = "arn:aws:acm:us-east-1:135367859851:certificate/1032b155-22da-4ae0-9f69-e206f825458b"
   #     ssl_support_method  = "sni-only"
   #   }
-  default_root_object = "index.html"
+  default_root_object        = "index.html"
+
+
 
   custom_error_response = [{
     error_caching_min_ttl = 10
